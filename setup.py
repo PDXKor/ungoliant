c4fe703a4cadd1e5cb9ef24d08cf2b86a99a5b9b
PDXKor <kmstafford1@gmail.com>
2025-02-05 07:22:17 -0800

    resturcture

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ungoliant",
+    version="0.1.0",
+    packages=find_packages(),
+    description="",
+    author="",
+    author_email="your.email@example.com",
+    url="",  # Optional
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
